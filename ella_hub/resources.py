050b90f40a758cd5f84d1c15ba51a36ae0720f2c
Mišo Belica <miso.belica@gmail.com>
2012-07-30 14:22:58 +0200

    `Blank` attribute in `ApiModelResource` is set to real value of `blank` attribute of `django.db.models.Model` (https://github.com/toastdriven/django-tastypie/issues/377).

--- ella_hub/resources.py
+++ ella_hub/resources.py
@@ -2,6 +2,71 @@ from tastypie.resources import ModelResource
 
 
 class ApiModelResource(ModelResource):
+    @classmethod
+    def get_fields(cls, fields=None, excludes=None):
+        """
+        Given any explicit fields to include and fields to exclude, add
+        additional fields based on the associated model.
+
+        Fixes #377 at https://github.com/toastdriven/django-tastypie/issues/377
+        This method should be removed when issue #377 will be fixed in PyPI.
+        """
+        final_fields = {}
+        fields = fields or []
+        excludes = excludes or []
+
+        if not cls._meta.object_class:
+            return final_fields
+
+        for f in cls._meta.object_class._meta.fields:
+            # If the field name is already present, skip
+            if f.name in cls.base_fields:
+                continue
+
+            # If field is not present in explicit field listing, skip
+            if fields and f.name not in fields:
+                continue
+
+            # If field is in exclude list, skip
+            if excludes and f.name in excludes:
+                continue
+
+            if cls.should_skip_field(f):
+                continue
+
+            api_field_class = cls.api_field_from_django_field(f)
+
+            kwargs = {
+                'attribute': f.name,
+                'help_text': f.help_text,
+                'blank': f.blank,
+            }
+
+            if f.null is True:
+                kwargs['null'] = True
+
+            kwargs['unique'] = f.unique
+
+            if not f.null and f.blank is True:
+                kwargs['default'] = ''
+
+            if f.get_internal_type() == 'TextField':
+                kwargs['default'] = ''
+
+            if f.has_default():
+                kwargs['default'] = f.default
+
+            if getattr(f, 'auto_now', False):
+                kwargs['default'] = f.auto_now
+
+            if getattr(f, 'auto_now_add', False):
+                kwargs['default'] = f.auto_now_add
+
+            final_fields[f.name] = api_field_class(**kwargs)
+            final_fields[f.name].instance_name = f.name
+
+        return final_fields
+
     def alter_list_data_to_serialize(self, request, data):
         """
         Returns only field `objects` with usefull data.
