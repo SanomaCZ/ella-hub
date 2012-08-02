703cd8c18e503ff552044280e068008b398ef509
Michal Belica <miso.belica@gmail.com>
2012-08-02 13:30:25 +0200

    Login/logout users before requests. Created sample user for tests.

--- tests/test_api/test_get_resources.py
+++ tests/test_api/test_get_resources.py
@@ -6,14 +6,26 @@ import django.utils.simplejson as json
 
 from nose import tools
 from django.test.client import Client
+from django.contrib.auth.models import User
 from ella.utils.test_helpers import create_basic_categories, create_and_place_a_publishable
 
 
 class TestGetResources(unittest.TestCase):
     def setUp(self):
+        self.user = self.__create_test_user("user", "pass")
         self.client = Client()
         create_basic_categories(self)
         create_and_place_a_publishable(self)
+
+    def tearDown(self):
+        self.user.delete()
+
+    def __create_test_user(self, username, password, is_admin=False):
+        user = User.objects.create_user(username=username, password=password)
+        user.is_staff = True
+        user.is_superuser = is_admin
+        user.save()
+        return user
 
     def test_ella_resources_present(self):
         response = self.client.get("/admin-api/")
@@ -51,29 +63,57 @@ class TestGetResources(unittest.TestCase):
         """
         Meta informations from returned JSON should be removed.
         """
-        response = self.client.get("/admin-api/article/")
+        api_key = self.__login("user", "pass")
+        headers = self.__build_headers("user", api_key)
+
+        response = self.client.get("/admin-api/article/", **headers)
         resources = self.__get_response_json(response)
 
         tools.assert_true(isinstance(resources, list))
         for object in resources:
             tools.assert_true(isinstance(object, dict))
 
+        self.__logout(headers)
+
     def test_url_filed_in_publishables_present(self):
         """
         URL field should be added in every publishable object.
         """
-        response = self.client.get("/admin-api/publishable/")
+        api_key = self.__login("user", "pass")
+        headers = self.__build_headers("user", api_key)
+
+        response = self.client.get("/admin-api/publishable/", **headers)
         resources = self.__get_response_json(response)
 
         for object in resources:
             tools.assert_true("url" in object)
 
         # article should inherit every publishable field
-        response = self.client.get("/admin-api/article/")
+        response = self.client.get("/admin-api/article/", **headers)
         resources = self.__get_response_json(response)
 
         for object in resources:
             tools.assert_true("url" in object)
+
+        self.__logout(headers)
+
+    def __login(self, username, password):
+        response = self.client.post('/admin-api/login/', data={"username": username, "password": password})
+        tools.assert_equals(response.status_code, 200)
+
+        resources = self.__get_response_json(response)
+        tools.assert_true("api_key" in resources)
+
+        return resources["api_key"]
+
+    def __logout(self, headers):
+        response = self.client.post('/admin-api/logout/', **headers)
+        tools.assert_equals(response.status_code, 302)
+
+    def __build_headers(self, username, api_key):
+        return {
+            "HTTP_AUTHORIZATION" : "ApiKey %s:%s" % (username, api_key),
+        }
 
     def __get_response_json(self, response):
         return json.loads(response.content)
