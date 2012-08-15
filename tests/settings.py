d2f7bd15c7bc91658ca6bbd6c1722bbdb347350d
vlado <vladimirbrigant@hotmail.com>
2012-08-15 15:15:28 -0400

    Django-guardian app removed.

--- tests/settings.py
+++ tests/settings.py
@@ -134,7 +134,6 @@ INSTALLED_APPS = (
     'ella.photos',
     'ella.articles',
 
-    'guardian',
     'tastypie',
     'ella_hub',  # install it via python setup.py develop
 )
@@ -143,15 +142,6 @@ INSTALLED_APPS = (
 RESOURCE_MODULES = (
     'ella_hub.ella_resources',
 )
-
-# django-guardian auth. backends
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'guardian.backends.ObjectPermissionBackend',
-)
-
-# django-guardian anonym. user
-ANONYMOUS_USER_ID = -1
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
