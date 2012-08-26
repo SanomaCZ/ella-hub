45a05162c69612fc9c12833f2fb5617f11dd6a68
vlado <vladimirbrigant@hotmail.com>
2012-08-26 19:42:57 -0400

    Registration of 'view' model-level permission added.

--- tests/example_project/urls.py
+++ tests/example_project/urls.py
@@ -10,7 +10,7 @@ from ella_hub.api import EllaHubApi
 api = EllaHubApi(api_name='admin-api')
 collected_resources = api.collect_resources()
 api.register_resources(collected_resources)
-
+#api.register_view_model_permission()
 
 urlpatterns = patterns('',
     # enable admin
