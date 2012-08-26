6f6bd1a8d0572423fc75482cd4f7c08bfc5d7332
vlado <vladimirbrigant@hotmail.com>
2012-08-26 19:38:48 -0400

    Altering resource schema based on user's permissions.

--- ella_hub/resources.py
+++ ella_hub/resources.py
@@ -1,12 +1,28 @@
-from tastypie.resources import ModelResource
+import re
 
+from tastypie import fields
+from tastypie.resources import ModelResource
+from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+
+import ella_hub.api
 from ella_hub.auth import ApiAuthentication as Authentication
 from ella_hub.auth import ApiAuthorization as Authorization
+from ella_hub.utils.perms import has_user_model_perm, has_obj_perm
 # from tastypie.authentication import Authentication
 # from tastypie.authorization import Authorization
 
 
+class HttpResponseForbidden(HttpResponse):
+    def __init__(self):
+        super(HttpResponseForbidden, self).__init__(status=403)
+
+
 class ApiModelResource(ModelResource):
+
+    _patch = fields.BooleanField(attribute="PATCH", help_text="Can change resource.", default=True)
+    _delete = fields.BooleanField(attribute="DELETE", help_text="Can delete resource.", default=True)
+
     @classmethod
     def get_fields(cls, fields=None, excludes=None):
         """
@@ -72,14 +88,81 @@ class ApiModelResource(ModelResource):
 
         return final_fields
 
-    def alter_list_data_to_serialize(self, request, data):
+    def get_schema(self, request, **kwargs):
         """
-        Returns only field `objects` with usefull data.
+        Overriding get_schema method because of altering resource schema
+        based on user permissions.
         """
-        if isinstance(data, dict) and "objects" in data:
-            return data["objects"]
-        else:
-            return data
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.is_authorized(request)
+        self.throttle_check(request)
+        self.log_throttled_access(request)
+        schema = self.build_schema()
+
+        methods_prefixes = (
+            ("view_", "get"),
+            ("add_", "post"),
+            ("change_", "put"),
+            ("change_", "patch"),
+            ("delete_", "delete"),
+        )
+        
+        user_perms = request.user.get_all_permissions()
+
+        (objects_class_name,) = re.match(r"/[^/]*/([^/]*)/.*", request.path).groups()
+        model_name = ella_hub.api.get_model_name(objects_class_name)
+        ct = ContentType.objects.get(model=model_name)
+
+        allowed_methods = []
+
+        for (prefix, method) in methods_prefixes:
+            permission_string = prefix + model_name
+            if (ct.app_label + '.' + permission_string) in user_perms:
+                allowed_methods.append(method)
+            
+        if not allowed_methods:
+            return HttpResponseForbidden()
+
+        schema['allowed_detail_http_methods'] = allowed_methods
+        schema['allowed_list_http_methods'] = allowed_methods
+        # applying only fields allowed for logged user,
+        #del schema['fields']['app_data']
+        # plus specifying readonly attribute
+        #schema['fields']['app_data']['readonly'] = True
+        return self.create_response(request, schema)
+
+
+    def alter_list_data_to_serialize(self, request, bundle):
+        """
+        Returns only field `objects` with useful data.
+        """
+        bundle = super(ApiModelResource, self).alter_list_data_to_serialize(request, bundle)
+        if isinstance(bundle, dict) and "objects" in bundle:
+            bundle = bundle["objects"]
+        
+        for object in bundle:
+            self.__filter_according_to_perms(request, object)
+        return bundle
+
+    def alter_detail_data_to_serialize(self, request, bundle):
+        bundle = super(ApiModelResource, self).alter_detail_data_to_serialize(request, bundle)
+        return self.__filter_according_to_perms(request, bundle)
+
+    def __filter_according_to_perms(self, request, bundle):
+        resource_name = bundle.obj.__class__.__name__.lower()
+
+        if (not has_user_model_perm(request.user, resource_name, 'change') and not
+            has_obj_perm(request.user, bundle.obj, 'change_%s' % resource_name)):
+            bundle.data['_patch'] = False
+
+        if (not has_user_model_perm(request.user, resource_name, 'delete') and not
+            has_obj_perm(request.user, bundle.obj, 'delete_%s' % resource_name)):
+            bundle.data['_delete'] = False
+
+        #filter according to user's permissions, f.e.:
+        #   del object.data['content']
+        return bundle
 
     class Meta:
         authentication = Authentication()
