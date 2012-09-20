9ed917c603564c3b5a580840863073fcc01a6a41
Mišo Belica <miso.belica@gmail.com>
2012-09-20 09:27:41 +0200

    Added "X-Mime-Type" header to allowed headers.

--- ella_hub/utils/middleware.py
+++ ella_hub/utils/middleware.py
@@ -21,8 +21,20 @@ class CrossDomainAccessMiddleware(object):
     __OPTIONS = (
         # (header type, settings variable, default value)
         ("Origin", "XS_SHARING_ALLOWED_ORIGINS", "*"),
-        ("Methods", "XS_SHARING_ALLOWED_METHODS", ("POST", "GET", "OPTIONS", "PUT", "PATCH", "DELETE")),
-        ("Headers", "XS_SHARING_ALLOWED_HEADERS", ("Content-Type", "X-Requested-With", "Authorization")),
+        ("Methods", "XS_SHARING_ALLOWED_METHODS", (
+            "POST",
+            "GET",
+            "OPTIONS",
+            "PUT",
+            "PATCH",
+            "DELETE",
+        )),
+        ("Headers", "XS_SHARING_ALLOWED_HEADERS", (
+            "Content-Type",
+            "X-Requested-With",
+            "Authorization",
+            "X-Mime-Type",
+        )),
         ("Credentials", "XS_SHARING_ALLOWED_CREDENTIALS", "true"),
     )
 
