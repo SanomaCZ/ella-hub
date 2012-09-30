deaf29d59d8e79d2b4f4bcdfb1dedb7996d0078b
vlado <vladimirbrigant@hotmail.com>
2012-09-30 16:48:23 -0400

    Workflow & Permission models added.

--- ella_hub/admin.py
+++ ella_hub/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-
 from ella.core.admin import PublishableAdmin, ListingInlineAdmin, RelatedInlineAdmin
+
 from ella_hub.models import Draft, CommonArticle, Encyclopedia, Recipe, RecipeIngredient
+from ella_hub.models import Permission, Role, ModelPermission, PrincipalRoleRelation
+from ella_hub.models import (Workflow, State, Transition, StatePermissionRelation,
+    StateModelRelation, WorkflowModelRelation, WorkflowPermissionRelation)
 
 
 class ArticleAdmin(PublishableAdmin):
@@ -71,3 +74,26 @@ admin.site.register(Draft)
 admin.site.register(CommonArticle, ArticleAdmin)
 admin.site.register(Encyclopedia, EncyclopediaAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+
+
+class PermissionAdmin(admin.ModelAdmin):
+    exclude = ("content_types",)
+
+
+admin.site.register(Permission, PermissionAdmin)
+admin.site.register(Role)
+admin.site.register(ModelPermission)
+
+
+class PrincipalRoleRelationAdmin(admin.ModelAdmin):
+    exclude = ("content_type", "content_id")
+
+admin.site.register(PrincipalRoleRelation, PrincipalRoleRelationAdmin)
+
+admin.site.register(Workflow)
+admin.site.register(State)
+admin.site.register(Transition)
+admin.site.register(StatePermissionRelation)
+admin.site.register(StateModelRelation)
+admin.site.register(WorkflowModelRelation)
+admin.site.register(WorkflowPermissionRelation)
