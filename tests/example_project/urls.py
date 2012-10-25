8c05c4bd50edeb59fd80aa291b9b45b4a3e3ec3d
Mišo Belica <miso.belica@gmail.com>
2012-10-25 13:03:53 +0200

    Changed routing.

--- tests/example_project/urls.py
+++ tests/example_project/urls.py
@@ -1,29 +1,24 @@
 from django.contrib import admin
-from django.conf.urls.defaults import patterns, include, url, handler404, handler500
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.generic.simple import direct_to_template
-
 from ella_hub.api import EllaHubApi
 from ella_hub.utils.workflow import init_ella_workflow
 
 admin.autodiscover()
 
 # admin API setup
-admin_api = EllaHubApi("admin-api")
+admin_api = EllaHubApi('admin-api')
 resources = admin_api.collect_resources()
 admin_api.register_resources(resources)
 init_ella_workflow(resources)
 
 
 urlpatterns = patterns('',
-    # enable admin
     url(r'^admin/', include(admin.site.urls)),
-
-    (r'^', include(admin_api.urls)),
-    (r'^admin-hope/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': 'admin-hope',
-        'show_indexes': True,
-    }),
-
-    (r'^', include('ella.core.urls')),
-) + staticfiles_urlpatterns()
+    url(r'^', include(admin_api.urls)),
+    url(r'^', include('ella.core.urls')),
+)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
--- tests/urls.py
+++ tests/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 
 from ella_hub.api import EllaHubApi
 from ella_hub.utils.workflow import init_ella_workflow
 
 # admin API setup
-admin_api = EllaHubApi("admin-api")
+admin_api = EllaHubApi('admin-api')
 resources = admin_api.collect_resources()
 admin_api.register_resources(resources)
 init_ella_workflow(resources)
 
 
 urlpatterns = patterns('',
-    (r'^', include(admin_api.urls)),
-    (r'^', include('ella.core.urls')),
+    url(r'^', include(admin_api.urls)),
+    url(r'^', include('ella.core.urls')),
 )
