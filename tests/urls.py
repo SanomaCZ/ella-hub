b1a2308e29cbe02c0c14cc661d887d68950154ab
vlado <vladimirbrigant@hotmail.com>
2012-11-14 01:05:28 -0500

    Removed ella workflow init (caused inconsistencies with test workflows).

--- tests/urls.py
+++ tests/urls.py
@@ -7,7 +7,6 @@ from ella_hub.utils.workflow import init_ella_workflow
 admin_api = EllaHubApi('admin-api')
 resources = admin_api.collect_resources()
 admin_api.register_resources(resources)
-init_ella_workflow(resources)
 
 
 urlpatterns = patterns('',
