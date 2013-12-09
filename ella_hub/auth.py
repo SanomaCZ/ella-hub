b0050bd523dea090e894a0a8729c517032cc8bc9
Eso <esoeso@gmail.com>
2013-12-09 14:36:30 +0100

    some more auth checks added to read_list and read_detail methods

--- ella_hub/auth.py
+++ ella_hub/auth.py
@@ -50,3 +50,29 @@ class ApiAuthorization(DjangoAuthorization):
     """
     # Regular Expression parsing resource name from `request.path`.
     __re_objects_class = re.compile(r"/[^/]*/(?P<resource_name>[^/]*)/.*")
+
+    def read_list(self, object_list, bundle):
+        klass = self.base_checks(bundle.request, object_list.model)
+
+        if klass is False:
+            return []
+
+        permission = '%s.change_%s' % (klass._meta.app_label, klass._meta.module_name)
+
+        if not bundle.request.user.has_perm(permission):
+            raise Unauthorized("You are not allowed to access that resource.")
+
+        return object_list
+
+    def read_detail(self, object_list, bundle):
+        klass = self.base_checks(bundle.request, bundle.obj.__class__)
+
+        if klass is False:
+            raise Unauthorized("You are not allowed to access that resource.")
+
+        permission = '%s.change_%s' % (klass._meta.app_label, klass._meta.module_name)
+
+        if not bundle.request.user.has_perm(permission):
+            raise Unauthorized("You are not allowed to access that resource.")
+
+        return True
