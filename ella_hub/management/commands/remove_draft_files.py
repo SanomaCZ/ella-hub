1a4342dde3630745a11094a6ba53384e85a2038d
michal dub <michal.dub@sanomamedia.cz>
2016-07-13 15:46:44 +0200

    fixed clean command if path does not exists

--- ella_hub/management/commands/remove_draft_files.py
+++ ella_hub/management/commands/remove_draft_files.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 
 from django.core.management import BaseCommand
@@ -9,5 +10,9 @@ class Command(BaseCommand):
     help = "Remove draft files created during upload photos"
 
     def handle(self, *args, **options):
-        shutil.rmtree(get_media_drafts_root())
-        print('Tmp files removed')
+        drafts_dir = get_media_drafts_root()
+        if os.path.exists(drafts_dir) and os.path.isdir(drafts_dir):
+            shutil.rmtree(drafts_dir)
+            print('Tmp files removed')
+        else:
+            print("Path %s does not exists or last part is not directory" % drafts_dir)
